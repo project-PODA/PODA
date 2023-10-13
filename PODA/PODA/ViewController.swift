fdca8cc222a04874401b2eec2d80bb11a97bab9b
Kyle <kyle.pxrk@gmail.com>
2023-10-13 11:34:20 +0900

    [SETTING]: 초기 세팅 스토리보드 삭제

---
+++ PODA/PODA/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PODA
+//
+//  Created by Kyle on 2023/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
