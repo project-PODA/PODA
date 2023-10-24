5f1af56717a2450ddc5309faf895123c73a4afc4
Kyle <kyle.pxrk@gmail.com>
2023-10-24 19:59:53 +0900

    [FIX]: TestPage관련 코드주석처리

--- PODA/PODA/Screen/Piece/ViewController/PieceViewController.swift
+++ PODA/PODA/Screen/Piece/ViewController/PieceViewController.swift
@@ -54,11 +54,11 @@ class PieceViewController: BaseViewController, UIConfigurable {
         $0.backgroundColor = Palette.podaGray5.getColor()
     }
     
-    let testPageButton = UIButton().then {
-        $0.setUpButton(title: "불러오기 테스트", podaFont: .body2, cornerRadius: 5)
-        $0.setTitleColor(Palette.podaWhite.getColor(), for: .normal)
-        $0.backgroundColor = Palette.podaGray5.getColor()
-    }
+//    let testPageButton = UIButton().then {
+//        $0.setUpButton(title: "불러오기 테스트", podaFont: .body2, cornerRadius: 5)
+//        $0.setTitleColor(Palette.podaWhite.getColor(), for: .normal)
+//        $0.backgroundColor = Palette.podaGray5.getColor()
+//    }
     
     // MARK: LifeCycle
     
@@ -88,7 +88,7 @@ class PieceViewController: BaseViewController, UIConfigurable {
         view.addSubview(addToGalleryButton)
         view.addSubview(memoryDate)
         view.addSubview(datePickerButton)
-        view.addSubview(testPageButton)
+//        view.addSubview(testPageButton)
         
         cancelButton.snp.makeConstraints {
             $0.left.equalToSuperview().offset(20)
@@ -131,12 +131,12 @@ class PieceViewController: BaseViewController, UIConfigurable {
             $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
         }
         
-        testPageButton.snp.makeConstraints {
-            $0.right.equalToSuperview().offset(-20)
-            $0.width.equalTo(108)
-            $0.height.equalTo(44)
-            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
-        }
+//        testPageButton.snp.makeConstraints {
+//            $0.right.equalToSuperview().offset(-20)
+//            $0.width.equalTo(108)
+//            $0.height.equalTo(44)
+//            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-50)
+//        }
     }
     
     func setGesture() {
@@ -154,7 +154,7 @@ class PieceViewController: BaseViewController, UIConfigurable {
         cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
         addToGalleryButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
         datePickerButton.addTarget(self, action: #selector(showDatePicker), for: .touchUpInside)
-        testPageButton.addTarget(self, action: #selector(testPageButtonTapped), for: .touchUpInside)
+//        testPageButton.addTarget(self, action: #selector(testPageButtonTapped), for: .touchUpInside)
     }
     
     func updateUIForImageAvailability(hasImage: Bool) {
@@ -213,9 +213,9 @@ class PieceViewController: BaseViewController, UIConfigurable {
         showSaveConfirmationAlert()
     }
     
-    @objc func testPageButtonTapped() {
-        present(TestPageViewController(), animated: true)
-    }
+//    @objc func testPageButtonTapped() {
+//        present(TestPageViewController(), animated: true)
+//    }
     
     @objc func addButtonTapped() {
         var configuration = PHPickerConfiguration()
