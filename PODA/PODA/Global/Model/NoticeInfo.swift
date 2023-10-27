1128bd48d9f8a9451f42c0390bb7d11490630eb8
real-future <future.ver1@gmail.com>
2023-10-27 12:39:46 +0900

    [UI]: NoticePage-테이블뷰 내용부분 UI 세팅

--- PODA/PODA/Global/Model/NoticeInfo.swift
+++ PODA/PODA/Global/Model/NoticeInfo.swift
@@ -9,4 +9,9 @@ struct NoticeInfo: Codable {
     let title: String
     var date: String
     let content: String
+    var isContentVisible: Bool = false
+    
+    enum CodingKeys: String, CodingKey {
+        case title, date, content
+    }
 }
--- PODA/PODA/Screen/Info/ViewController/NoticeCell.swift
+++ PODA/PODA/Screen/Info/ViewController/NoticeCell.swift
@@ -1,97 +1,84 @@
-//
-//  NoticeTableViewCell.swift
-//  PODA
-//
-//  Created by FUTURE on 2023/10/24.
-//
-
+// MARK: - Import Statements
 import UIKit
 import SnapKit
-import MessageUI
 import Then
 
 class NoticeCell: UITableViewCell {
-    
+
+    // MARK: - Properties
     private var isExpanded: Bool = false
-    
+
     private let titleLabel = UILabel().then {
         $0.textColor = Palette.podaWhite.getColor()
         $0.font = .podaFont(.body2)
     }
-    
+
     private let dateLabel = UILabel().then {
         $0.textColor = Palette.podaGray3.getColor()
         $0.font = .podaFont(.body2)
     }
-    
-//    private let contentLabel = UILabel().then {
-//        $0.textColor = Palette.podaWhite.getColor()
-//        $0.font = .podaFont(.body2)
-//        $0.layer.backgroundColor = Palette.podaGray5.getColor().cgColor
-//        $0.numberOfLines = 0
-//    }
-    
-    //    private let disclosureImageView = UIImageView(image: UIImage(named: "icon_disclosure")).then {
-    //        $0.contentMode = .scaleAspectFit
-    //        $0.tintColor = Palette.podaWhite.getColor()
-    //    }
-    
+
+    private let contentBox = UIView().then {
+        $0.backgroundColor = Palette.podaGray5.getColor()
+    }
+
+    private let contentLabel = UILabel().then {
+        $0.font = .podaFont(.body2)
+        $0.textColor = Palette.podaWhite.getColor()
+        $0.numberOfLines = 0
+        $0.lineBreakMode = .byCharWrapping
+    }
+
+    // MARK: - Initializers
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupUI()
+        configUI()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func setupUI() {
-        
-        addSubview(titleLabel)
-        addSubview(dateLabel)
-//        addSubview(contentLabel)
-        
-        //        addSubview(disclosureImageView)
-        
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(10)
-            make.leading.equalToSuperview().offset(15)
+
+    // MARK: - configUI
+    private func configUI() {
+        // Adding subviews
+        [titleLabel, dateLabel, contentBox].forEach {
+            contentView.addSubview($0)
         }
-        
-        dateLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(12)
-            make.leading.equalTo(titleLabel)
+        contentBox.addSubview(contentLabel)
+
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalToSuperview().offset(20)
+            $0.height.equalTo(20)
         }
-        
-//        contentLabel.snp.makeConstraints { make in
-//            make.top.equalTo(dateLabel.snp.bottom).offset(10)
-//            make.leading.equalToSuperview()
-//            make.trailing.equalToSuperview()
-//            make.bottom.equalToSuperview().offset(-10)
-//        }
-        
-//        contentLabel.isHidden = true  // 기본적으로 숨깁니다.
-        
-        //
-        //        disclosureImageView.snp.makeConstraints { make in
-        //            make.centerY.equalToSuperview()
-        //            make.trailing.equalToSuperview().offset(-15)
-        //            make.size.equalTo(CGSize(width: 6, height: 11))
-        //        }
+
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(titleLabel)
+            $0.height.equalTo(20)
+        }
+
+        contentBox.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(0)
+            $0.bottom.equalToSuperview().offset(-10)
+        }
+
+        contentLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(20)
+        }
+
+        contentBox.isHidden = true
     }
-    
-    func configure(title: String, date: String) {
-        titleLabel.text = "[공지] \(title)"
+
+    // MARK: - funcs
+    func configure(title: String, date: String, content: String, isContentVisible: Bool) {
+        titleLabel.text = title
         dateLabel.text = date
-//        contentLabel.text = content
+        contentLabel.text = content
 
-//        self.isExpanded = isExpanded
-//        contentLabel.isHidden = !isExpanded
+        contentBox.isHidden = !isContentVisible
+        contentLabel.isHidden = !isContentVisible
     }
-
-//    func contentLabelHeight() -> CGFloat {
-//        let width = contentView.frame.width
-//        let size = contentLabel.sizeThatFits(CGSize(width: width, height: CGFloat.greatestFiniteMagnitude))
-//        return size.height
-//    }
 }
