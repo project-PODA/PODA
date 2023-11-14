89f223b4efb3d57a000f4f2d9377cc6a68874288
2rang2 <rang0825@gmail.com>
2023-11-14 20:17:57 +0900

    [FIX]: 탭바에 QR코드 촬영기능 추가

--- PODA/PODA/Global/Base/BaseTabbarController.swift
+++ PODA/PODA/Global/Base/BaseTabbarController.swift
@@ -11,17 +11,21 @@ import SnapKit
 class BaseTabbarController: UITabBarController {
     
     private let customTabbarView = UIView()
+    private let qrContainerView = UIView()
     private let homeContainerView = UIView()
     private let profileContainerView = UIView()
+    private let qrImageView = UIImageView()
     private let homeImageView = UIImageView()
     private let profileImageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let homeVM = HomeViewModel(firebaseDBManager: FirestorageDBManager(), firebaseImageManager: FireStorageImageManager(imageManipulator: ImageManipulator()))
-        let homeVC = HomeViewController(viewModel: homeVM)
-        homeVC.bind(to: homeVC.viewModel)
+        let qrViewContorller = QRViewController()
+        
+        let homeViewModel = HomeViewModel(firebaseDBManager: FirestorageDBManager(), firebaseImageManager: FireStorageImageManager(imageManipulator: ImageManipulator()))
+        let homeViewController = HomeViewController(viewModel: homeViewModel)
+        homeViewController.bind(to: homeViewController.viewModel)
         
         
 //        let viewModel = ProfileViewModel(fireDBManager: FirestorageDBManager(), fireImageManager: FireStorageImageManager(imageManipulator: ImageManipulator()))
@@ -31,13 +35,18 @@ class BaseTabbarController: UITabBarController {
         
         let fireAuthManager = FireAuthManager(firestorageDBManager: FirestorageDBManager(), firestorageImageManager: FireStorageImageManager(imageManipulator: ImageManipulator()))
         
-        let infoVC = InfoViewController(viewModel: InfoViewModel(fireAuthManager: fireAuthManager))
-        
-        let infoNavVC = BaseNavigationController(rootViewController: infoVC)
+        let infoViewController = InfoViewController(viewModel: InfoViewModel(fireAuthManager: fireAuthManager))
+    
+        let infoNavViewController = BaseNavigationController(rootViewController: infoViewController)
 
-        infoVC.bind(to: infoVC.viewModel)
+        infoViewController.bind(to: infoViewController.viewModel)
         
-        viewControllers = [homeVC, infoNavVC]
+        viewControllers = [HomeMenuViewController(), homeViewController, infoNavViewController]
+        
+        //viewControllers = [qrViewContorller, homeViewController, infoNavViewController]
+        
+        // HomeViewController를 초기 실행될 앱으로 지정
+        selectedIndex = 1
         
         setupCustomTabbar()
         updateTabbarImages()
@@ -45,10 +54,16 @@ class BaseTabbarController: UITabBarController {
     
     private func updateTabbarImages() {
         switch self.selectedIndex {
-        case 0: // 홈
+        case 0: // QR
+            qrImageView.image = UIImage(named: "icon_qrCode.selected")
+            homeImageView.image = UIImage(named: "icon_home")
+            profileImageView.image = UIImage(named: "icon_person")
+        case 1: // 홈
+            qrImageView.image = UIImage(named: "icon_qrCode")
             homeImageView.image = UIImage(named: "icon_home.selected")
             profileImageView.image = UIImage(named: "icon_person")
-        case 1: // 마이페이지
+        case 2: // 마이페이지
+            qrImageView.image = UIImage(named: "icon_qrCode")
             homeImageView.image = UIImage(named: "icon_home")
             profileImageView.image = UIImage(named: "icon_person.selected")
         default:
@@ -62,32 +77,43 @@ class BaseTabbarController: UITabBarController {
         customTabbarView.layer.cornerRadius = 40
         view.addSubview(customTabbarView)
         
-        [homeContainerView, profileContainerView].forEach {
+        [qrContainerView, homeContainerView, profileContainerView].forEach {
             $0.layer.cornerRadius = 35
             $0.isUserInteractionEnabled = true
             customTabbarView.addSubview($0)
         }
         
+        qrContainerView.addSubview(qrImageView)
         homeContainerView.addSubview(homeImageView)
         profileContainerView.addSubview(profileImageView)
         
         customTabbarView.snp.makeConstraints {
             $0.height.equalTo(80)
-            $0.width.equalTo(200)
+            $0.width.equalTo(280)
             $0.bottom.equalTo(view.snp.bottom).offset(-20)
             $0.centerX.equalToSuperview()
         }
         
-        homeContainerView.snp.makeConstraints {
+        qrContainerView.snp.makeConstraints {
             $0.height.width.equalTo(60)
             $0.left.equalTo(customTabbarView.snp.left).offset(7.5)
             $0.centerY.equalToSuperview()
+        }
+        
+        homeContainerView.snp.makeConstraints {
+            $0.height.width.equalTo(60)
+            $0.center.equalToSuperview()
         }
         
         profileContainerView.snp.makeConstraints {
             $0.height.width.equalTo(60)
             $0.right.equalTo(customTabbarView.snp.right).offset(-7.5)
             $0.centerY.equalToSuperview()
+        }
+        
+        qrImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(60)
         }
         
         homeImageView.snp.makeConstraints {
@@ -99,6 +125,9 @@ class BaseTabbarController: UITabBarController {
             $0.center.equalToSuperview()
             $0.width.height.equalTo(60)
         }
+        
+        let qrTapGesture = UITapGestureRecognizer(target: self, action: #selector(qrTapped))
+        qrContainerView.addGestureRecognizer(qrTapGesture)
         
         let homeTapGesture = UITapGestureRecognizer(target: self, action: #selector(homeTapped))
         homeContainerView.addGestureRecognizer(homeTapGesture)
@@ -117,14 +146,18 @@ class BaseTabbarController: UITabBarController {
         }
     }
     
-    
-    @objc private func homeTapped() {
+    @objc private func qrTapped() {
         self.selectedIndex = 0
         updateTabbarImages()
     }
     
-    @objc private func profileTapped() {
+    @objc private func homeTapped() {
         self.selectedIndex = 1
+        updateTabbarImages()
+    }
+    
+    @objc private func profileTapped() {
+        self.selectedIndex = 2
         updateTabbarImages()
     }
 }
